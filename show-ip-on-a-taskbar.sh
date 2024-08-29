ab07d49361402cfca4964c711e3fe2596bee3aa0
JauniusDobro <87813074+JauniusDobro@users.noreply.github.com>
2024-08-29 14:10:07 +0300

    Create show-ip-on-a-taskbar.sh

---
+++ show-ip-on-a-taskbar.sh
@@ -0,0 +1,27 @@
+#!usr/bin/env bash
+
+# Install Zenity
+
+sudo apt update
+sudo apt install zenity -y
+
+
+# Create .sh file
+echo "#!usr/bin/env bash
+
+# Add delay 
+sleep 10
+
+# Get local IP
+local_ip=$(hostname -I)
+
+# Display the local IP
+zenity --info --title="Your Local IP Address" --text="Your Local IP Address is: $local_ip"
+
+" > show_ip.sh
+
+# Change permissions
+chmod +x show_ip.sh
+
+# Execute the file
+./show_ip.sh
